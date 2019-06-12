35623adbcd21f12fdc0fd1243381903161a5784b
davieu <davisumana@gmail.com>
2019-06-12 12:06:38 -0400

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item active>Quiz Your Mind</b-nav-item>
+      <b-nav-item>Counter 4/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+ 
+<style>
+
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Question Box</h1>
+</template>
+
+<script>
+  export default {
+    name: 'Question box'
+  }
+</script>
+
+<style>
+
+</style>
+
+
